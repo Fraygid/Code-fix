cdfbfd50a50f2ea7b351b00fc03b258c85626512
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-02-02 12:56:22 +0300

    Update №6.py

--- №6.py
+++ №6.py
@@ -1,1 +1,15 @@
-sedth
+mx = -10**7
+s = 0
+count_neg = 0
+for i in range(10): #
+    x = int(input())
+    if x < 0:
+        s += x #
+        if x > mx:
+            mx = x
+        count_neg += 1
+if count_neg == 0:
+    print('NO')
+else:
+    print(s)
+    print(mx)
