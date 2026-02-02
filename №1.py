3c0fc3fcdb3e3da72948fd6a42b4a3b04e06459b
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-02-02 11:59:26 +0300

    Update №1.py

--- №1.py
+++ №1.py
@@ -1,1 +1,7 @@
-asd
+n = int(input()) #Добавлено int(), иначе выводит ошибку
+product = 1 #Начальное значение для произведения
+if n > 0: #Обрабатываем все цифры, включая последнюю
+    digit = n % 10
+    product = product * digit
+    n //= 10
+print(product)
