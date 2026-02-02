b012c38751bfec149cecd8870bddcce8025a5596
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-02-02 12:44:09 +0300

    Create #5.5 "Очень странные никнеймы".py

---
+++ #5.5 "Очень странные никнеймы".py
@@ -0,0 +1,12 @@
+s = input()
+
+while len(s) < 10:
+    if len(s) % 4 == 0:
+        s = s + 'y'
+    elif len(s) % 5 == 0:
+        s = s + 'y'
+    else:
+        s = 'z' + s
+
+s = '@' + s
+print(s)
